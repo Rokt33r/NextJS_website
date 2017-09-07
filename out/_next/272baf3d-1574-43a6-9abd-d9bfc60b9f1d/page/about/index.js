5c38a827e672be1d8ee5195228b76cc190290490
Junyoung Choi <fluke8259@gmail.com>
2017-09-07 23:27:21 +0900

    Delete generated stuff :fire:

--- out/_next/272baf3d-1574-43a6-9abd-d9bfc60b9f1d/page/about/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/about', function() {
-            var comp = module.exports=webpackJsonp([6],{355:function(e,t,u){e.exports=u(356)},356:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var l=u(6),a=n(l),o=u(115),r=(n(o),u(183)),f=(n(r),u(117)),c=n(f);t.default=function(){return a.default.createElement(c.default,null,a.default.createElement("p",null,"This is the about page"))}}},[355]);
-            return { page: comp.default }
-          })
-        
